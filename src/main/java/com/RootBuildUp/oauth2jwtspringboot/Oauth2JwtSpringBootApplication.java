9e8ee062f9dbeada8c93b5ee3da55a85e1a5ab54
rashedfci994 <rashed@businessaccelerate.com.bd>
2021-10-20 00:39:20 +0600

    first commit

---
+++ src/main/java/com/RootBuildUp/oauth2jwtspringboot/Oauth2JwtSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.RootBuildUp.oauth2jwtspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2JwtSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2JwtSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RootBuildUp/oauth2jwtspringboot/Oauth2JwtSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RootBuildUp.oauth2jwtspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2JwtSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
